a349be7263f49a8e6c39a29ee1e3ce9cb5590e7c
Trey Cox <tcox@beeline.com>
2022-12-30 11:59:47 -0500

    adding game logging logic

--- src/components/ViewSession.vue
+++ src/components/ViewSession.vue
@@ -1,21 +1,32 @@
 <template>
   <h2>Sessions</h2>
-  <section v-for="session in sessions">
+  <section v-for="session in sessions" >
+    <section v-if="showGames == null || session.sessionTime == showGames">
     <h3>{{ session.sessionLocation }}</h3>
     <p>{{ session.sessionTime }}</p>
+    <button @click="showGames = session.sessionTime">View games</button>
+    <section v-if="session.sessionTime == showGames">
+      <ViewGames v-bind:Session="session"/>
+    </section>
+  </section>
   </section>
 </template>
 <script>
 import axios from "axios";
+import ViewGames from "./ViewGames.vue";
 
 export default {
   name: "ViewSessions",
-  props: ["league"],
+  props: ["league", ],
+  components: {
+    ViewGames
+  },
   data() {
     return {
       sessions: null,
       showCreateSession: false,
       showViewSessions: false,
+      showGames: null
     };
   },
   methods: {
